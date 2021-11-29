1833ecdadba14233fa008424059fdade44be2cee
Musfiqur Rahma <musfiq371@gmail.com>
2021-11-30 02:13:15 +0600

    added introduce

--- src/pages/Home/Body/Body.js
+++ src/pages/Home/Body/Body.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import pcImg from '../../../Image/graphicstock-student-sitting-at-the-table-with-laptop-student-using-laptop-for-education-man-working-on-laptop-and-writing-notes-educational-technology-concept-vector-flat-design-illustration-square-l.png'
-import Typical from 'react-typical'
+import Typical from 'react-typical';
+import './Body.css';
 
 const Body = () => {
     return (
-         <div style={{backgroundColor:'green',height:'100vh'}}>
-            <Container sm={12} md={6}>
+         <div className='body-container'>
+            <Container>
             <Row>
-               <Col>
-                  <h2>Hi There!</h2>
-                        <h2>I'M <span>MD MUSFIQUR ROHOMAN</span></h2>
-                        <h3>
+               <Col sm={12} md={7}>
+                  <h2 style={{color:'#ffffff'}}>Hi There!</h2>
+                        <h1 style={{color:'white'}}>I'M <span style={{color:'#a14dc4'}}>MD MUSFIQUR ROHOMAN</span></h1>
+                        <h1 style={{color:'#a14dc4'}}>
                             <Typical
-                        loop={Infinity}
-                        steps={[
+                            loop={Infinity}
+                            steps={[
                             "Javascript Lover",
                             4000, 
                             "Full Stack Developer",
@@ -26,13 +27,44 @@ const Body = () => {
                             "Deep Learner",
                             4000, 
                         ]}
-                        /></h3>
+                        /></h1>
                </Col>
-               <Col>
-                   <img style={{height:'100%'}} src={pcImg} alt="" />       
+               <Col sm={12} md={5}>
+                   <img style={{height:'120%'}} src={pcImg} alt="" />       
                </Col>
-            </Row>
-           </Container>
+                </Row>
+                <Row style={{marginTop:'30%'}}>
+                    <Col sm={12} md={7}>
+                        <h1>LET ME
+                            <span style={{ color: '#c770f0' }}>INTRODUCE</span>
+                            MYSELF</h1>
+
+                        <h4>I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️</h4>
+                        <br />
+
+                        <h4>I am fluent in classics like
+                            <i style={{ color: '#c770f0' }}>Javascript,React,MongoDb,Node.js,Express
+                            </i>
+                        </h4>
+                        <br />
+
+                        <h4>My field of Interest's are building new <i
+                            style={{ color: '#c770f0' }}>Web Technologies and Products</i>
+                            and also in areas related to
+                            <i style={{ color: '#c770f0' }}>Deep Learning and Natural Launguage Processing.
+                            </i>
+                        </h4>
+                        <h4>Whenever possible, I also apply my passion for developing products with
+                            <i style={{ color: '#c770f0' }}>Node.js</i>
+                            and
+                            <i style={{ color: '#c770f0' }}>Modern Javascript Library and Frameworks
+                            </i> like
+                            <i style={{ color: '#c770f0' }}>React.js and varities packages.</i>
+                        </h4>
+                    </Col>
+                   <Col sm={12} md={5}>sm=4</Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/pages/Home/Resume/Resume.js
+++ src/pages/Home/Resume/Resume.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button,} from 'react-bootstrap';
+import Navigation from '../Navigation/Navigation';
+import './Resume.css';
+
 
 const Resume = () => {
     return (
         <div>
-            <a href="Resume-new(2).pdf" download="Resume-new(2).pdf">
+            <Navigation></Navigation>
+            <div className='resume-container'>
+            <a href='Resume-new(2).pdf' download='Resume-new(2).pdf'>
             <Button>Download Resume</Button>
-           </a>
+            </a>
+            </div>
         </div>
     );
 };
