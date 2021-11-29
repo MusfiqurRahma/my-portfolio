f8ca30078299e986882244e73e3c09fda958b8ae
Musfiqur Rahma <musfiq371@gmail.com>
2021-11-29 21:58:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home/Home/Home';
+import About from './pages/Home/About/About';
+import Resume from './pages/Home/Resume/Resume';
+import Contact from './pages/Home/Contact/Contact';
+import Blogs from './pages/Home/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home></Home>}>
+          </Route>
+          <Route path='/home' element={<Home></Home>}>
+          </Route>
+          <Route path='/about' element={<About></About>}>
+          </Route>
+          <Route path='/resume' element={<Resume></Resume>}>
+          </Route>
+          <Route path='/contactMe' element={<Contact></Contact>}>
+          </Route>
+          <Route path='/blog' element={<Blogs></Blogs>}>
+          </Route>
+         </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/Home/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logoImg from '../../../Image/letter-mr-logo-colorful-splash-background-combination-design-creative-industry-web-business-company-203791253.jpg';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar style={{backgroundColor:'#005d70'}} variant="light" sticky='top' collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home"><img style={{height:'60px'}} src={logoImg} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-center navLeft">
+                        <Nav className='text-primary'>
+                            <Link className='text-light me-5 fw-bold nav-list' to="/home">Home</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' to="/about">About</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' to="/projects">Projects</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' to="/resume">Resume</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' to="/contactMe">Contact</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' to="/blog">Blog</Link>
+                            </Nav>                           
+                            </Navbar.Collapse>                   
+                </Container>
+            </Navbar>    
+        </div>
+    );
+};
+
+export default Navigation;
