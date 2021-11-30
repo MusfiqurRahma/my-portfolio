2a12b8c5bd4fa3b118c1b686350b51a30bd3c105
Musfiqur Rahma <musfiq371@gmail.com>
2021-12-01 05:28:24 +0600

    added icon

--- src/pages/Home/Navigation/Navigation.js
+++ src/pages/Home/Navigation/Navigation.js
@@ -13,12 +13,12 @@ const Navigation = () => {
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-center navLeft">
                         <Nav className='text-primary'>
-                            <Link className='text-light me-5 fw-bold nav-list'style={{textDecoration:'none'}} to="/home">Home</Link>
-                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/about">About</Link>
-                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/projects">Projects</Link>
-                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/resume">Resume</Link>
-                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/contact">Contact</Link>
-                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/blog">Blog</Link>
+                            <Link className='text-light me-5 fw-bold nav-list'style={{textDecoration:'none'}} to="/home"><i class="fas fa-house-damage"></i> Home</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/about"><i class="fas fa-user-tie"></i> About</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/projects"><i class="fas fa-tv"></i> Projects</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/resume"><i class="fas fa-file-alt"></i> Resume</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/contact"><i class="fas fa-mail-bulk"></i> Contact</Link>
+                            <Link className='text-light fw-bold me-5 nav-list' style={{textDecoration:'none'}} to="/blog"><i class="fas fa-blog"></i> Blog</Link>
                             </Nav>                           
                             </Navbar.Collapse>                   
                 </Container>
